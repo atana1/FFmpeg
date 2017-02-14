831fc781d68ddfb9871a94b5ccd39ed130fc45c2
Atana <atana@openmailbox.org>
2017-02-15 03:30:11 +0530

    mode added

--- libavfilter/af_peakpoints2.c
+++ libavfilter/af_peakpoints2.c
@@ -53,6 +53,7 @@ typedef struct {
 typedef struct {
     const AVClass *class;
     double *data;
+    int mode;
     int nsamples;
     int index;
     size_t time;
@@ -434,6 +435,7 @@ static int getPeakPoints2(AVFilterContext *ctx, PeakPointsContext *ppc) {
 
 static const AVOption peakpoints2_options[] = {
     { "wsize",  "set window size", OFFSET(windowSize),  AV_OPT_TYPE_INT,    {.i64=1024},    0, INT_MAX},
+    { "mode", "do lookup for the song or store the song", OFFSET(mode), AV_OPT_TYPE_INT, {.i64=1}, 0, 1},
     { NULL },
 };
 
@@ -446,6 +448,10 @@ static av_cold int init(AVFilterContext *ctx)
     if (p->windowSize < 1024 || p->windowSize > SIZECHECK) {
         av_log(ctx, AV_LOG_ERROR, "window size must be in range 16 to %d\n", SIZECHECK);
         return AVERROR(EINVAL);
+    }
+
+    if ((p->mode != 0) && (p->mode != 1)) {
+        av_log(ctx, AV_LOG_ERROR, "mode is either 0 or 1; 0 to store, 1 to lookup\n");
     }
 
     av_log(ctx, AV_LOG_INFO, "window size is %d\n", p->windowSize);
@@ -537,50 +543,52 @@ static void ppointsStats(AVFilterContext *ctx, PeakPointsContext *p) {
             av_log(ctx, AV_LOG_INFO, "%d:%d:%d:%d:%zu:%zu:%zu\n", f1,
                     f2, f3, f4, t2-t1, t3-t2, t4-t3);
 
-            // put to file db
-            buf_size = sizeof(entry);
+            if (!p->mode) {
+                // put to file db
+                buf_size = sizeof(entry);
 
-            bytestream2_init_writer(&pb, entry, buf_size);
-            // write data to putbyte context
-            for (j = 0; j < 8; j++) {
-                bytestream2_put_le16(&pb, p->cpoints[i+j].frequency);
-                /*if (j != 7) {
-                    bytestream2_put_le16(pb, ',');
-                }*/
+                bytestream2_init_writer(&pb, entry, buf_size);
+                // write data to putbyte context
+                for (j = 0; j < 8; j++) {
+                    bytestream2_put_le16(&pb, p->cpoints[i+j].frequency);
+                    /*if (j != 7) {
+                        bytestream2_put_le16(pb, ',');
+                    }*/
+                }
+
+                //bytestream2_put_le16(pb, ':');
+
+                for (j = 0; j < 8; j++) {
+                    bytestream2_put_le16(&pb, p->cpoints[i+j].time);
+                    /*if (j != 7) {
+                        bytestream2_put_le16(pb, ',');
+                    }*/
+                }
+
+                // just for test song_id = 1
+                song_id = 1;
+                //bytestream2_put_le16(pb, ':');
+                bytestream2_put_le16(&pb, song_id);
+                //bytestream2_put_le16(pb, '\n');
+
+                //filename is same as anchor point
+                length = snprintf( NULL, 0, "%d", f1);
+                length = length + snprintf(NULL, 0, "PPDB-%d", f1);
+                filename = malloc(length + 1); //extra 1 for terminating character
+                snprintf(filename, length + 1, "PPDB-%d", f1);
+
+                fp = avpriv_open(filename, O_RDWR|O_CREAT|O_APPEND, 0666);
+
+                //check
+                if (fp == -1) {
+                    av_log(ctx, AV_LOG_ERROR, "Cann't open file %s for writing\n", filename);
+                }
+
+                write(fp, entry, buf_size);
+
+                close(fp);
+                av_freep(&filename);
             }
-
-            //bytestream2_put_le16(pb, ':');
-
-            for (j = 0; j < 8; j++) {
-                bytestream2_put_le16(&pb, p->cpoints[i+j].time);
-                /*if (j != 7) {
-                    bytestream2_put_le16(pb, ',');
-                }*/
-            }
-
-            // just for test song_id = 1
-            song_id = 1;
-            //bytestream2_put_le16(pb, ':');
-            bytestream2_put_le16(&pb, song_id);
-            //bytestream2_put_le16(pb, '\n');
-
-            //filename is same as anchor point
-            length = snprintf( NULL, 0, "%d", f1);
-            length = length + snprintf(NULL, 0, "PPDB-%d", f1);
-            filename = malloc(length + 1); //extra 1 for terminating character
-            snprintf(filename, length + 1, "PPDB-%d", f1);
-
-            fp = avpriv_open(filename, O_RDWR|O_CREAT|O_APPEND, 0666);
-
-            //check
-            if (fp == -1) {
-                av_log(ctx, AV_LOG_ERROR, "Cann't open file %s for writing\n", filename);
-            }
-
-            write(fp, entry, buf_size);
-
-            close(fp);
-            av_freep(&filename);
         }
 
         // free peaks and set size to zero
